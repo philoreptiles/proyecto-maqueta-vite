bd8b4d96feb48ed42a5cc8fdcda4c3f5bfd77067
Alfonso López <lopezl.luisalfonso@gmail.com>
2026-09-05 14:17:24 -0600

    fix: agregar vite.config.js para paginas secundarias

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+﻿import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        nosotros: resolve(__dirname, 'src/pages/nosotros.html'),
+        admin: resolve(__dirname, 'src/pages/admin.html'),
+      },
+    },
+  },
+});
